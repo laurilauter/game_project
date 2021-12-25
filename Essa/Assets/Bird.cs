dc5599b666b7fa26a2d0b5b98cad27dc7aeb253b
laurilautervikk <lauri.lauter@vikk.ee>
2021-12-25 22:41:13 +0200

    initial

---
+++ Essa/Assets/Bird.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    private Vector3 _initialPosition;
+    private bool _birdWasLounched;
+    private float _timeSittingAround;
+
+    [SerializeField] private float _launchPower = 300;
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        GetComponent<LineRenderer>().SetPosition(1, _initialPosition);
+        
+
+        if (_birdWasLounched &&
+            GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+        {
+            _timeSittingAround += Time.deltaTime;
+        }
+
+        if (transform.position.y > 10 ||
+            transform.position.y < -10 ||
+            transform.position.y > 10 ||
+            transform.position.y < -10 ||
+            _timeSittingAround > 2)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+
+
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        //turn on line renderer
+        GetComponent<LineRenderer>().enabled = true;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdWasLounched = true;
+
+        GetComponent<LineRenderer>().enabled = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition  = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
+    }
+
+}
